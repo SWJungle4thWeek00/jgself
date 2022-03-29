698f4800e49e309be182aa15e754ab92306887df
sinterhoo <56079377+sinterhoo@users.noreply.github.com>
2022-03-29 23:34:20 +0900

    Add go to uploadPage

--- app.py
+++ app.py
@@ -112,6 +112,10 @@ def logout():
     session.pop('userId', None)
     return redirect('/')
 
+@app.route('/upload', methods=['GET'])
+def getUpload():
+    return render_template('upload.html')
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
